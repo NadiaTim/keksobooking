bf9deb54cab614003e524854d6292366335e89dc
Надежда <timakovanadezda0@gmail.com>
2025-01-28 01:43:37 +0300

    Добавлена функция получения целого случайного числа

--- js/main.js
+++ js/main.js
@@ -1,1 +1,20 @@
+/**
+ * Получение целого случайного числа в заданном диапозоне
+ * @param {number} min - Минимальное значение диапазона (включительно)
+ * @param {number} max - Максимальное значение диапазона (включительно)
+ * @returns {number} Случайное целое число
+ */
+const getRandomInteger = (min, max) => {
+  if (min < 0 || max < 0) {
+    return -1;
+  }
+
+  if (max < min) {
+    [min, max] = [max, min];
+  }
+
+  return Math.floor(Math.random() * (max - min + 1))+ min;
+};
+
+getRandomInteger(5, 8);
 
