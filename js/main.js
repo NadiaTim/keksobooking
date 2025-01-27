706be8dc4a9629cad52ceecc861940d7c0db8b45
Надежда <timakovanadezda0@gmail.com>
2025-01-28 01:44:33 +0300

    Добавлена функция получения числа с плавающей точкой из диапазона

--- js/main.js
+++ js/main.js
@@ -18,3 +18,22 @@ const getRandomInteger = (min, max) => {
 
 getRandomInteger(5, 8);
 
+/**
+ * Получение случайного числа из диапазона с несколькими занками после запятой
+ * @param {number} min  - Минимальное значение диапазона (включительно)
+ * @param {number} max - Максимальное значение диапазона (включительно)
+ * @param {number} digits - Количество знаков после запятой (по умолчанию - 0)
+ * @returns {number} число с плавающей точкой из заданного диапазона
+ */
+const getRundomFloat = function (min, max, digits = 0) {
+  if (min < 0 || max < 0 || digits < 0) {
+    return -1;
+  }
+
+  if (max < min) {
+    [min, max] = [max, min];
+  }
+
+  return parseFloat((Math.random() * (max - min) + min).toFixed(digits));
+};
+getRundomFloat(4.499, 4.5, 3);
