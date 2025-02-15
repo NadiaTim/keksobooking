7e68b7ea717d9bc98a8a4c71de3e477be9b92a56
Надежда <timakovanadezda0@gmail.com>
2025-02-15 16:34:50 +0300

    добавлены функции получения и отправки данных

---
+++ js/fetch.js
@@ -0,0 +1,45 @@
+
+//Создайте новый модуль
+// и опишите в нём функции
+// взаимодействия удалённым сервером
+// с помощью fetch
+// для получения
+//https://23.javascript.htmlacademy.pro/keksobooking/data
+//
+// и отправки данных
+//https://23.javascript.htmlacademy.pro/keksobooking (post)
+
+const SERVER = 'https://23.javascript.htmlacademy.pro/keksobooking';
+const DATA = 'https://23.javascript.htmlacademy.pro/keksobooking/data';
+
+const getData = (onSuccess, onFail) => {
+  fetch(DATA)
+    .then((response) => response.json())
+    .then((adverts) => {
+      onSuccess(adverts)
+    })
+    .catch(() => {
+      //добавить обработку на отсутствие данных с сервера
+      //или отсутствие подключения к нему
+      onFail('Не удалось получить данные');
+    });
+};
+
+const sendData = (onSuccess, onFail) => {
+  fetch(
+    SERVER, {
+      method: 'POST',
+    })
+    .then((response) => {
+      if (response.ok) {
+        onSuccess();
+      } else {
+        onFail('Не удалось отправить форму. Попробуйте еще раз');
+      }
+    })
+    .catch(() => {
+      onFail('Не удалось отправить форму. Попробуйте ещё раз.');
+    });
+};
+
+export { getData, sendData };
